2107493cd0f2a41ae947866c2a30ae93b61f5ff9
RaymondYan38 <raymondyan17@gmail.com>
2023-10-22 18:18:50 -0700

    completed login functionality

---
+++ project1/src/LoginServlet.java
@@ -0,0 +1,69 @@
+import com.google.gson.JsonObject;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.io.IOException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
+public class LoginServlet extends HttpServlet {
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        JsonObject responseJsonObject = new JsonObject();
+
+        try (Connection conn = dataSource.getConnection()) {
+            String query = "SELECT id FROM customers WHERE email = ? AND password = ?";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                // Login success
+                request.getSession().setAttribute("user", new User(username));
+                responseJsonObject.addProperty("status", "success");
+                responseJsonObject.addProperty("message", "success");
+            } else {
+                // Login fail
+                responseJsonObject.addProperty("status", "fail");
+                responseJsonObject.addProperty("message", "Incorrect username or password");
+            }
+            resultSet.close();
+            preparedStatement.close();
+            response.getWriter().write(responseJsonObject.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            // Write error message responseJsonObject object
+            responseJsonObject.addProperty("status", "fail");
+            responseJsonObject.addProperty("message", "Database error");
+
+            // Set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        }
+    }
+}
