461e1bdedb56190bd5d9eca1ad3205e79925dc87
RaymondYan38 <raymondyan17@gmail.com>
2023-11-12 18:44:05 -0800

    commented out broken trigger

--- project1/createtable.sql
+++ project1/createtable.sql
@@ -26,13 +26,13 @@ CREATE TABLE IF NOT EXISTS stars_in_movies (
 );
 
 # Trigger helps maintain count of movies for stars
-CREATE TRIGGER updateMoviesCount AFTER INSERT ON stars_in_movies
-FOR EACH ROW
-BEGIN
-    UPDATE stars
-    SET numMovies = numMovies+1
-    WHERE id = NEW.starId;
-END;
+# CREATE TRIGGER updateMoviesCount AFTER INSERT ON stars_in_movies
+# FOR EACH ROW
+# BEGIN
+#     UPDATE stars
+#     SET numMovies = numMovies+1
+#     WHERE id = NEW.starId;
+# END;
 
 CREATE TABLE IF NOT EXISTS genres (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
