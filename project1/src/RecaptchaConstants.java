18afb2f630edb1e60a8495e8769b5faa1bee5277
Radhakrishna Vempati <vempatir@uci.edu>
2023-11-12 23:57:10 -0800

    recapactha

--- project1/src/RecaptchaConstants.java
+++ project1/src/RecaptchaConstants.java
@@ -1,4 +1,8 @@
 public class RecaptchaConstants {
-    public static final String SECRET_KEY ="6Ld_9-YoAAAAAP2IAV1njzgdXoSf0oObk19ndjKI";
-
+    //RAYMOND's KEY
+    //public static final String SECRET_KEY ="6Ld_9-YoAAAAAP2IAV1njzgdXoSf0oObk19ndjKI
+    //
+    // ";
+    //KRISHNA's KEY
+    public static final String SECRET_KEY ="6Ld9LA0pAAAAAJ5bEp_U-vY4A8NMRznXNINNL1nV";
 }
