ec5d79d18c9fb0046c4880b835a2ddf773caef3f
RaymondYan38 <raymondyan17@gmail.com>
2023-11-13 21:39:03 -0800

    krishna stuff

--- project1/src/RecaptchaConstants.java
+++ project1/src/RecaptchaConstants.java
@@ -1,6 +1,6 @@
 public class RecaptchaConstants {
     //RAYMOND's KEY
-    public static final String SECRET_KEY ="6Ld_9-YoAAAAAP2IAV1njzgdXoSf0oObk19ndjKI";
+//    public static final String SECRET_KEY ="6Ld_9-YoAAAAAP2IAV1njzgdXoSf0oObk19ndjKI";
     //KRISHNA's KEY
-//    public static final String SECRET_KEY ="6Ld9LA0pAAAAAJ5bEp_U-vY4A8NMRznXNINNL1nV";
+    public static final String SECRET_KEY ="6Ld9LA0pAAAAAJ5bEp_U-vY4A8NMRznXNINNL1nV";
 }
